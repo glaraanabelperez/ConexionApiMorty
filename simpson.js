36f3c463eb5be3ffd296936f799bce072fe668fd
glaraanabelperez <glaraanabelperez@gmail.com>
2020-12-15 15:30:47 -0300

    conexion nueva

--- simpson.js
+++ simpson.js
@@ -1,70 +1,61 @@
-const listadoImg=document.querySelector("#listImagenes");
-const listNom=document.querySelector("#listNom");
-const resultTitulo=document.querySelector("#resultTitulo");
-const imgBoton=document.querySelector("#imgBoton");
-
-//KER MARVEL da670a4e672828d1946716ee1fedbc71;
-// DIRECCION MARVEL https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=10&apikey=
-
+let info;
+let cardFinal = '';
 const input=document.querySelector("#buscador");
-async function buscarImagenes(){
-    const url=`https://thesimpsonsquoteapi.glitch.me/quotes?count=20`;
-    //const url=`https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=10&apikey=18467844-755bb86481e5df0833ad9f142`;
-    const data=await fetch(url);
-    const response=await data.json();
-    console.log(response);
-    const lista=response;
 
-    let titulo=document.createElement("div");
-        titulo.innerHTML=`
-        <p>Esta busqueda solo tiene los siguientes presonajes disponibles:</p>
-        <p>Si no ah encontrado su personaje, vuelva a iniciar la busqueda</p>
-        `
-        resultTitulo.appendChild(titulo);
+fetch('https://rickandmortyapi.com/api/character')
+  .then( response => {
+    if(response.status == 200) {
+      return response.text();
+    } else {
+      throw "Respuesta incorrecta del servidor" 
+    }
+  })
+  .then( responseText => {
+    let personajes = JSON.parse(responseText).results;
+    console.log('Este es el objeto: ', personajes);
+    info=personajes;
 
-    lista.forEach(element=>{
-        var a = (element.character).toString();
-        var separa = a.split(" ");
-        var a= separa[0] ;
-
-        let div2=document.createElement("div");
-        div2.innerHTML=`
-        <p>${a}  /   </p>
-        `
-        let button = document.createElement("button");
-        div2.appendChild(button);
-        button.innerText="+";
-        button.style.setProperty("border", "none"); 
-
-        button.addEventListener("click", function(){
-            console.log("si!!");
+    for( i=0; i<info.length; i++){
+            const lista = document.querySelector('#listaNombres');
             let div=document.createElement("div");
             div.innerHTML=`
-            <br>
-            <img  src=${element.image}></img>
+            <ul>
+                <li class="listaN">
+                    <a style="text-decoration:none" onclick="inputName">
+                    ${info[i].name}
+                    </a>
+                </li>
+            </ul> 
             `
-            div.style.setProperty("font-size", "5px")
-            imgBoton.appendChild(div);
-        });
-        listNom.appendChild(div2);
+            lista.appendChild(div);
+    }
+  })
 
-        var b = (input.value).toString();
-        console.log( "variable a: ", a);
-        console.log("variable b: ", b);
-        const encontrado=false;
+  .catch( err => {
+    console.log(err);
+  });
+ 
+function buscarImagenes(){
+    let card = '';
+    console.log("Datos", info);
+    var b = (input.value).toString();
+
+    if(b===""){
+        card += '<h5 class="lead">Por favor ingrese nombre completo!</h5>';
+    }
+    for (var i = 0; i < info.length; i++) {
         
-        if(a==b && encontrado==false){
-            console.log("si!!");
-            let div=document.createElement("div");
-            div.innerHTML=`
-            <h1>${element.character}</h1> 
-            <img  src=${element.image}></img>
-            </a>
-            `
-            listadoImg.appendChild(div);
-            this.encontrado=true;
+        if( info[i].name.toString() === b ){
+            card +='<br>';
+            card +='<img class="card-img " src="' +info[i].image+ '">';
+            card += '<div class="card-body">';
+            card += '<h5 class="card-title">'+ info[i].name +'</h5>';
+            card += '<a href="'+info[i].image +'" class="btn btn-primary" target="_blank" >Go!</a>';
+            card += '</div>';
+            $('#empresas').html(card);
         }
-    })
-}
-
+        $('#listImagenes').html(card);
+        cardFinal=card;
+    }
+ }
 
